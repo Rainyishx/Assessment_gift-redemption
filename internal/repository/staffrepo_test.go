380556e2f7d7d7cc2a54aec482caace5b55a831e
Rainyishx <ruiyun@berkeley.edu>
2026-04-16 15:49:16 -0700

    implement testing for staffrepo

---
+++ internal/repository/staffrepo_test.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"testing"
+)
+
+func TestSatffRepo(t *testing.T) {
+	repo, err := NewStaffRepository("../../data/staffmapping.csv")
+	if err != nil {
+		t.Fatal("Failed to initalise repo:", err)
+	}
+
+	//finding existing staff member
+	t.Run("Valid Staff ID", func(t *testing.T) {
+		teamName, found := repo.FindTeamByStaffPassID("STAFF_001")
+
+		if !found {
+			t.Errorf("expected to find STAFF_001, but did not")
+		}
+		if teamName != "TEAM_A" {
+			t.Errorf("expected to find TEAM_A, got '%s'", teamName)
+		}
+	})
+
+	//finding staff that doesn't exist
+	t.Run("Invalid Staff ID", func(t *testing.T) {
+		_, found := repo.FindTeamByStaffPassID("unknown")
+		if found {
+			t.Errorf("not expected to find unknown, but did")
+		}
+
+	})
+}
