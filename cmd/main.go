396d2516e199b8da0edbf494b61a7b580fdf7998
Rainyishx <rainredcloud@gmail.com>
2026-04-17 03:04:46 -0700

    feat: implemented the main application startup

---
+++ cmd/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"Assessment_gift-redemption/internal/handler"
+	"Assessment_gift-redemption/internal/repository"
+	"Assessment_gift-redemption/internal/service"
+	"log"
+	"net/http"
+	"os"
+)
+
+// returns the value of the env var key or fallback val if var not set
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
+
+func main() {
+	//configuration
+	staffMappingFile := getEnv("STAFF_MAPPING_FILE", "data/staffmapping.csv")
+	redempFile := getEnv("REDEMP_FILE", "data/redemptions.csv")
+	port := getEnv("PORT", "8080")
+
+	//initialise the data repo
+	staffRepo, err := repository.NewStaffRepository(staffMappingFile)
+	if err != nil {
+		//kills the program if fails to load csv
+		log.Fatalf("failed to load staff mappings from %s: %v", staffMappingFile, err)
+	}
+	log.Printf("staff mappings loaded from %s", staffMappingFile)
+
+	redempRepo, err := repository.NewRedempRepo(redempFile)
+	if err != nil {
+		log.Fatalf("failed to load redemptions from %s: %v", redempFile, err)
+	}
+	log.Printf("Redemption data loaded from %s", redempFile)
+
+	//initialise service
+	svc := service.NewRedempService(staffRepo, redempRepo)
+
+	//initialise handler
+	h := handler.NewHandler(svc)
+
+	//set up router
+	mux := http.NewServeMux()
+	h.RegisterRoutes(mux)
+
+	//start web server
+	addr := ":" + port
+	log.Printf("Gift Redemption API running on http://localhost%s", addr)
+	log.Printf("Routes:")
+	log.Printf(" GET /health")
+	log.Printf(" POST /redeem")
+
+	//ListenAndServe blocks main thread forvever, waiting for incoming traffic
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
+}
